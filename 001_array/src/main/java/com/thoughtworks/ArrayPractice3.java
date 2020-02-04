853a6c9688aa2d7ec575c91a06d4c782eaf46c6a
GraceeFlower <1491644103@qq.com>
2020-02-05 00:55:21 +0800

    [WM] feat: filter the zeros in the given array

--- 001_array/src/main/java/com/thoughtworks/ArrayPractice3.java
+++ 001_array/src/main/java/com/thoughtworks/ArrayPractice3.java
@@ -6,7 +6,20 @@ public class ArrayPractice3 {
      * 完成本方法实现功能: 将一个整型数组array中值0的元素去掉，并把剩下的元素组成新的数组
      */
     public static int[] filterZero(int[] array) {
-
-        return null;
+        int[] newArr;
+        int len = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (0 != array[i]) {
+                len++;
+            }
+        }
+        newArr = new int[len];
+        for (int i = 0, j = 0; i < array.length; i++) {
+            if (0 != array[i]) {
+                newArr[j] = array[i];
+                j++;
+            }
+        }
+        return newArr;
     }
 }
